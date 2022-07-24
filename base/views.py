e588ae552254582ec4bd5a81a9b8f10aa76ae0ee
sauravDutt <sauravd069@gmail.com>
2022-07-24 14:14:08 +0530

    Just added some more stuff

--- base/views.py
+++ base/views.py
@@ -1,3 +1,4 @@
+from multiprocessing import context
 from django.shortcuts import render
 
 # Create your views here.
@@ -10,8 +11,15 @@ rooms = [
 
 
 def home(request):
-    return render(request, 'home.html', {'rooms': rooms})
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
 
 
-def room(request):
-    return render(request, 'room.html')
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
