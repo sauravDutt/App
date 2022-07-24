08463a6486005f20c0154aade32d101d0daf993d
sauravDutt <sauravd069@gmail.com>
2022-07-24 16:51:19 +0530

    Just added some more stuff

--- base/admin.py
+++ base/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Post
+from .models import Post, Topic, Message
 
 admin.site.register(Post)
+admin.site.register(Topic)
+admin.site.register(Message)
 
